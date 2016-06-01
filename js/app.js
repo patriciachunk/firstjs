b1829fcf66da4bcc59d80438bbfa0168289c5ceb
Patricia Chun <yejeechun@gmail.com>
2016-06-01 15:28:23 -0400

    build the dl as an actual DOM element

--- js/app.js
+++ js/app.js
@@ -1,46 +1,45 @@
 $(document).foundation()
 
-function addValuesToDetails() {
 
-}
-
+fucntion BasicForm() {
 var buildColorDiv = function(color) {
   return '<div style="height: 100px; width: 100px; background-color: ' +color + '"></div>'
+};
+
+var buildList = function(listValues) {
+  var dl = document.createElement('dl');
+  dl.style.border =  '1px solid red';
+  dl.innerHTML = buildListItem('First Name', listValues.firstName) +
+    buildListItem('Hair Color', listValues.hairColor) +
+    buildListItem('Age', listValues.age) +
+    buildListItem('Birthplace', listValues.birthplace);
+    return dl;
+};
+
+var buildListItem = function(term, definition) {
+  var li = ' \
+  <li> \
+    <dt>' + term + '</dt> \
+    <dd>' + definition + '</dd> \
+  </li>';
+  return li;
 };
 
 var addValuesToDetails = function(ev) {
   ev.preventDefault();
   var details = document.querySelector('div.details');
-  //details.innerHTML += '<p>' + this.firstName.value + '</p>';
-  var firstName = this.firstName.value;
   var hairColor = this.hairColor.value;
-  var age = this.age.value;
-  var birthplace = this.birthplace.value;
 
-  var colorDiv = buildColorDiv(hairColor);
-  //details.innerHTML += '<p>' + firstName + '</p>';
-  //details.innerHTML += '<p>' + hairColor + '</p>';
-  //details.innerHTML += '<p>' + age + '</p>';
-  //details.innerHTML += '<p>' + birthplace + '</p>';
+  var listValues = {
+    firstName: this.firstName.value,
+    age: this.age.value,
+    birthplace: this.birthplace.value,
+    hairColor: buildColorDiv(hairColor)
+  };
 
-
-  details.innerHTML += ' \
-  <dl> \
-    <dt>Name</dt> \
-    <dd>' + firstName + '</dd> \
-        \
-    <dt>Hair Color</dt> \
-    <dd>' + colorDiv + '</dd> \
-      \
-    <dt>Age</dt> \
-    <dd>' + age + '</dd> \
-      \
-    <dt>Birthplace</dt> \
-    <dd>' + birthplace + '</dd> \
-  </dl>';
-
+  details.appendChild(buildList(listValues));
 };
-
 
 var myForm = document.querySelector('form');
 myForm.onsubmit = addValuesToDetails;
+}
